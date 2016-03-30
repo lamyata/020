ea9cec689d016c187b3dfdd0506a6d0c6748eb8c
lamyata <muammargaddafi@abv.bg>
2016-03-30 05:36:53 +0300

    .

--- conn.cs
+++ conn.cs
@@ -1,20 +1,21 @@
 using System;
 using System.Linq;
+using System.Data.SqlClient;
 
 class Conn01{
  static void Main(){
 
  string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;";
- connString += @"AttachDbFilename=C:\proj\db\SportsStore.mdf";
+ connString += @"AttachDbFilename=C:\proj\db\SportsStore.mdf;";
  connString += "Integrated Security=True; Connect Timeout = 30";
 				
- using (SqlConnection connection = new SqlConnection(connectionString)) {    
+ using (SqlConnection connection = new SqlConnection(connString)) {    
 	connection.Open();
 	var myCommand = new SqlCommand("select * from products");
 	myCommand.Connection = connection;
 	using (var reader = myCommand.ExecuteReader()) {
 		while (reader.Read())
-			Console.WriteLine(myReader.GetValue(1));
+			Console.WriteLine(reader.GetValue(1));
 	}
  }
  Console.ReadLine();
